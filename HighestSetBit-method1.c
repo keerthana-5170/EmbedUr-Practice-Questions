bd924724e508897b20c9ae15321a51be05f84f27
Keerthana S <54321645+keerthana-5170@users.noreply.github.com>
2023-02-23 12:44:25 +0530

    Create HighestSetBit-method1.c

---
+++ HighestSetBit-method1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    long int count=0,num,rem,m,arr[10];
+    f("Enter a number:");
+    scanf("%ld",&num);
+    m=num;    
+    while(m>0)
+    {
+        rem = m%2;
+        arr[count]=rem;
+        m=m/2;
+        count++;
+        
+    }
+    for(int j=count-1;j>=0;j--){
+        if(arr[j]==1)
+        {
+            printf("msb=%0.f",pow(2,j));
+             return 0;
+        }
+
+    }
+   
+}
